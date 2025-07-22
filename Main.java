a7d0d26466c3d7276dad4bd06f8eddc46ae5f82e
Abdulazizaisari2 <Abdulazizaisari06@gmail.com>
2025-07-22 12:01:27 +0400

    Step 2.1: Created base Account class with deposit, withdraw, and displayInfo

---
+++ Main.java
@@ -0,0 +1,36 @@
+// Account.java
+class Account {
+    protected String accountNumber;
+    protected String holderName;
+    protected double balance;
+
+    public Account(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    public void displayAccountInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Holder Name: " + holderName);
+        System.out.println("Balance: " + balance);
+    }
+}
